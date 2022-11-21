afcdd30b297b1cfe60fede8ea0d637f557adf2a6
Bruno Gomes Santos <81108851+bruno-gomes-santos@users.noreply.github.com>
2022-11-21 17:37:48 -0300

    Alterações

--- main.js
+++ main.js
@@ -22,13 +22,13 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  // createCard("20/11", "domingo", createGame(`qatar`, `13:00`, `ecuador`)) +
+  createCard("20/11", "domingo", createGame(`qatar`, `0 x 2`, `ecuador`)) +
   createCard(
     "21/11",
     "segunda",
-    // createGame(`england`, `10:00`, `iran`) +
-    // createGame(`senegal`, `13:00`, `netherlands`) +
-    createGame(`united states`, `16:00`, `wales`)
+    createGame(`england`, `6 x 2`, `iran`) +
+      createGame(`senegal`, `0 x 2`, `netherlands`) +
+      createGame(`united states`, `16:00`, `wales`)
   ) +
   createCard(
     "22/11",
