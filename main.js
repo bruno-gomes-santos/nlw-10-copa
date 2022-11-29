413d45ae7f9659c39ea0c5727349810cd9a05d26
Bruno Gomes Santos <81108851+bruno-gomes-santos@users.noreply.github.com>
2022-11-29 13:26:46 -0300

    atualizações

--- main.js
+++ main.js
@@ -89,8 +89,8 @@ document.querySelector("#cards").innerHTML =
   createCard(
     "29/11",
     "terça",
-    createGame(`ecuador`, `0 x 0`, `senegal`) +
-      createGame(`netherlands`, `0 x 0`, `qatar`) +
+    createGame(`ecuador`, `0 x 1`, `senegal`) +
+      createGame(`netherlands`, `2 x 0`, `qatar`) +
       createGame(`iran`, `16:00`, `united states`) +
       createGame(`wales`, `16:00`, `england`)
   ) +
